3f9bae0644feb63c778abfb7b4ab384214ed7a86
GowthamR-omniforge <gowthamr@omniforge.in>
2026-05-16 22:10:03 +0530

    style(ui): improve style page ui

--- NammaVastra-master/app/src/main/java/com/example/namma_vastraself_employment/ui/screens/HomeScreen.kt
+++ NammaVastra-master/app/src/main/java/com/example/namma_vastraself_employment/ui/screens/HomeScreen.kt
@@ -1,5 +1,4 @@
 package com.example.namma_vastraself_employment.ui.screens
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
